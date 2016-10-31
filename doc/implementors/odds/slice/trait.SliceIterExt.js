54c29e005e84814abe435210196d26eb4fc351b2
goandylok <andylokandy@hotmail.com>
2016-10-31 20:09:26 +0800

    add must_use

--- doc/implementors/odds/slice/trait.SliceIterExt.js
+++ doc/implementors/odds/slice/trait.SliceIterExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["odds"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];implementors["arraydeque"] = [];
+implementors["odds"] = [];implementors["arraydeque"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
